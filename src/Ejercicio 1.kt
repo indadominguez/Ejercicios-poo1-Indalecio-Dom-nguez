2cb1c686c500e9eeb4e9a1c65bd1a06ba4f1ac63
dhindalecio <dhindalecio@iesrafaelalberti.es>
2025-01-30 20:01:53 +0100

    Empiezo a mandar los ejercicios, ya estaban casi todos realizados

--- src/Ejercicio 1.kt
+++ src/Ejercicio 1.kt
@@ -1,5 +1,21 @@
-class Rectanculo(){
+class Rectangulo(val base: Double,val altura: Double){
+
+    init {
+        require(base > 0) { "La base debe de ser mayor que 0"}
+        require(altura > 0){ "La altura debe de ser mayor que 0"}
+
+    }
 
 
+    fun area(): Double {
+        return base * altura
+    }
 
+    fun perimetro(): Double {
+        return 2 * (base + altura)
+    }
+
+    override fun toString(): String {
+        return "Rectángulo(base=$base, altura=$altura"
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
+
+fun main(){
+
+    val rectangulo1 = Rectangulo(4.0, 2.0)
+    val rectangulo2 = Rectangulo(7.0, 5.0)
+    val rectangulo3 = Rectangulo(8.0, 3.0)
+
+
+    println(rectangulo1)
+    println("Área: ${rectangulo1.area()}")
+    println("Perímetro: ${rectangulo1.perimetro()}\n")
+
+    println(rectangulo2)
+    println("Área: ${rectangulo2.area()}")
+    println("Perímetro: ${rectangulo2.perimetro()}\n")
+
+    println(rectangulo3)
+    println("Área: ${rectangulo3.area()}")
+    println("Perímetro: ${rectangulo3.perimetro()}\n")
 }
+
+
+
+
+
+
+
+
+
+
